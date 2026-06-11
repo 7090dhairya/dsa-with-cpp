3864982032fa3a5858ff8c273a060b74987a9105
7090dhairya <dhairya0923@gmail.com>
2026-06-11 17:18:42 +0530

    this is day 5

---
+++ dsa_day5/recursion2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void print(int num){
+    if(num == 0){
+        return;
+    }
+
+    print(num - 1);
+    cout << num << " ";
+}
+
+int main(){
+    int num;
+    cout << "Enter the number: ";
+    cin >> num;
+
+    print(num);
+}
