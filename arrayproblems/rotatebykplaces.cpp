5c232f651496f52161e3ef55e11e5fed46767d03
7090dhairya <dhairya0923@gmail.com>
2026-06-26 17:48:35 +0530

    rotate array by k places

---
+++ arrayproblems/rotatebykplaces.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+void leftRotate(int arr[], int n, int k)
+{
+    k = k % n;
+
+    int temp[k];
+
+    // Store first k elements
+    for(int i = 0; i < k; i++)
+    {
+        temp[i] = arr[i];
+    }
+
+    // Shift remaining elements
+    for(int i = k; i < n; i++)
+    {
+        arr[i - k] = arr[i];
+    }
+
+    // Copy temp to end
+    for(int i = 0; i < k; i++)
+    {
+        arr[n - k + i] = temp[i];
+    }
+}
+
+int main()
+{
+    int arr[] = {1,2,3,4,5,6,7};
+    int n = 7;
+    int k = 3;
+
+    leftRotate(arr,n,k);
+
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<" ";
+}
